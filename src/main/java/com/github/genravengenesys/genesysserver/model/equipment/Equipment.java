bf057d7e2c17ff166d8430d333e02e99d57899df
genraven1 <genraven1@gmail.com>
2022-07-10 09:25:31 -0700

    Update Encumbrance logic

--- src/main/java/com/github/genravengenesys/genesysserver/model/equipment/Equipment.java
+++ src/main/java/com/github/genravengenesys/genesysserver/model/equipment/Equipment.java
@@ -1,5 +1,6 @@
 package com.github.genravengenesys.genesysserver.model.equipment;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,20 @@ public abstract class Equipment {
     private String name;
     private String description;
     private Price price;
-    private Encumbrance encumbrance;
+    private int encumbrance;
+    private EquipmentSlot slot;
     private Rarity rarity;
+
+    public enum EquipmentSlot {
+        @JsonProperty("Main Hand")
+        MAIN_HAND,
+        @JsonProperty("Off Hand")
+        OFF_HAND,
+        @JsonProperty("Both Hands")
+        BOTH_HANDS,
+        @JsonProperty("Body")
+        BODY
+    }
 
     @Data
     @NoArgsConstructor
@@ -18,14 +31,6 @@ public abstract class Equipment {
     public static class Price {
         private int value = 0;
         private boolean restricted = false;
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Encumbrance {
-        private int value = 0;
-        private boolean worn = false;
     }
 
     @Data
